251dd93765975f1cc0a63dc625d61ea4d9d94c6b
Ethical-Hacking-Zone <55155544+Ethical-Hacking-Zone@users.noreply.github.com>
2020-06-23 11:25:14 +0800

    Update P-TS.sh

--- P-TS.sh
+++ P-TS.sh
@@ -11,7 +11,7 @@ read a1
 echo "Found All Requirements....."
 echo "Installing Requirements....."
 apt install figlet toilet python curl -y
-python3 -m pip install wheel requests bs4 libxml2 libxslt lxml lolcat
+python3 -m pip install wheel requests bs4 libxslt lxml lolcat
 echo This Tool Was Made By cYBER kNIGHT
 echo Requirements Installed.....
 echo Press Enter To Continue....
